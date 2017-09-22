7a52d62ae081a01d108cd1d02fcf444865ab8804
Alexandra STANCIU <alstanciu@pentalog.com>
2017-09-22 17:58:20 +0300

    landing page, newsletter, our culture, css, js updates

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 $(function() {
-	/* begin scrolling the page */
-	// $('a[href*=#]').on('click', function(e) {
-	// 	e.preventDefault();
-	// 	$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-	// });
+	var landingPHeight = $('#home').height(); 
+
+	$(window).scroll(function() {
+		var offset = $(window).scrollTop();
+		if(offset > landingPHeight) { 
+			$('#nav').show();
+		} else {
+			$('#nav').hide();
+		}
+	});
 
 	/* check if your browser is a mobile browser and initialize the carousel for small devices */
 	/* i've called them separatelly in order to maintain the aspect of the carousels from the mockups. */
